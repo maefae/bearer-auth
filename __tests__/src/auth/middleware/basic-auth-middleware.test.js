bd93ae24c6aed5e25d6d6b7e9e6dc6979aa9773b
Megan Faith Seibert-Hughes <meganfaithhughes@gmail.com>
2022-12-11 11:24:43 -0600

    Some tests pass and some don't

--- __tests__/src/auth/middleware/basic-auth-middleware.test.js
+++ __tests__/src/auth/middleware/basic-auth-middleware.test.js
@@ -1,13 +1,13 @@
-'use strict';
+"use strict";
 
 process.env.SECRET = "TEST_SECRET";
 
-const base64 = require('base-64');
-const middleware = require('../../../../src/auth/middleware/basic.js');
-const { db, users } = require('../../../../src/auth/models/index.js');
+const base64 = require("base-64");
+const middleware = require("../../../../src/auth/middleware/basic.js");
+const { db, users } = require("../../../../src/auth/models/index.js");
 
 let userInfo = {
-  admin: { username: 'admin-basic', password: 'password' },
+  admin: { username: "admin-basic", password: "password" },
 };
 
 // Pre-load our database with fake users
@@ -19,8 +19,7 @@ afterAll(async () => {
   await db.drop();
 });
 
-describe('Auth Middleware', () => {
-
+describe("Auth Middleware", () => {
   // admin:password: YWRtaW46cGFzc3dvcmQ=
   // admin:foo: YWRtaW46Zm9v
 
@@ -28,41 +27,38 @@ describe('Auth Middleware', () => {
   const req = {};
   const res = {
     status: jest.fn(() => res),
-    send: jest.fn(() => res)
-  }
+    send: jest.fn(() => res),
+  };
   const next = jest.fn();
 
-  describe('user authentication', () => {
-
-    it('fails a login for a user (admin) with the incorrect basic credentials', () => {
-      const basicAuthString = base64.encode('username:password');
+  describe("user authentication", () => {
+    it("fails a login for a user (admin) with the incorrect basic credentials", () => {
+      const basicAuthString = base64.encode("username:password");
 
       // Change the request to match this test case
       req.headers = {
         authorization: `Basic ${basicAuthString}`,
       };
 
-      return middleware(req, res, next)
-        .then(() => {
-          expect(next).not.toHaveBeenCalled();
-          expect(res.status).toHaveBeenCalledWith(403);
-        });
-
+      return middleware(req, res, next).then(() => {
+        expect(next).not.toHaveBeenCalled();
+        expect(res.status).toHaveBeenCalledWith(403);
+      });
     });
 
-    it('logs in an admin user with the right credentials', () => {
-      let basicAuthString = base64.encode(`${userInfo.admin.username}:${userInfo.admin.password}`);
+    it("logs in an admin user with the right credentials", () => {
+      let basicAuthString = base64.encode(
+        `${userInfo.admin.username}:${userInfo.admin.password}`
+      );
 
       // Change the request to match this test case
       req.headers = {
         authorization: `Basic ${basicAuthString}`,
       };
 
-      return middleware(req, res, next)
-        .then(() => {
-          expect(next).toHaveBeenCalledWith();
-        });
-
+      return middleware(req, res, next).then(() => {
+        expect(next).toHaveBeenCalledWith();
+      });
     });
   });
 });
--- __tests__/src/auth/middleware/bearer-auth-middleware.test.js
+++ __tests__/src/auth/middleware/bearer-auth-middleware.test.js
@@ -1,13 +1,12 @@
-'use strict';
+"use strict";
 
-process.env.SECRET = "TEST_SECRET";
-
-const bearer = require('../../../../src/auth/middleware/bearer.js');
-const { db, users } = require('../../../../src/auth/models/index.js');
-const jwt = require('jsonwebtoken');
+require("dotenv").config();
+const bearer = require("../../../../src/auth/middleware/bearer.js");
+const { db, users } = require("../../../../src/auth/models/index.js");
+const jwt = require("jsonwebtoken");
 
 let userInfo = {
-  admin: { username: 'admin', password: 'password' },
+  admin: { username: "admin", password: "password" },
 };
 
 // Pre-load our database with fake users
@@ -19,47 +18,39 @@ afterAll(async () => {
   await db.drop();
 });
 
-describe('Auth Middleware', () => {
-
+describe("Auth Middleware", () => {
   // Mock the express req/res/next that we need for each middleware call
   const req = {};
   const res = {
     status: jest.fn(() => res),
     send: jest.fn(() => res),
     json: jest.fn(() => res),
-  }
+  };
   const next = jest.fn();
 
-  describe('user authentication', () => {
-
-    it('fails a login for a user (admin) with an incorrect token', () => {
-
+  describe("user authentication", () => {
+    it("fails a login for a user (admin) with an incorrect token", () => {
       req.headers = {
-        authorization: 'Bearer thisisabadtoken',
+        authorization: "Bearer thisisabadtoken",
       };
 
-      return bearer(req, res, next)
-        .then(() => {
-          expect(next).not.toHaveBeenCalled();
-          expect(res.status).toHaveBeenCalledWith(403);
-        });
-
+      return bearer(req, res, next).then(() => {
+        expect(next).not.toHaveBeenCalled();
+        expect(res.status).toHaveBeenCalledWith(403);
+      });
     });
 
-    it('logs in a user with a proper token', () => {
-
-      const user = { username: 'admin' };
+    it("logs in a user with a proper token", () => {
+      const user = { username: "admin" };
       const token = jwt.sign(user, process.env.SECRET);
 
       req.headers = {
         authorization: `Bearer ${token}`,
       };
 
-      return bearer(req, res, next)
-        .then(() => {
-          expect(next).toHaveBeenCalledWith();
-        });
-
+      return bearer(req, res, next).then(() => {
+        expect(next).toHaveBeenCalledWith();
+      });
     });
   });
 });
